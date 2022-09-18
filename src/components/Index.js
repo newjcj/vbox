9431a8cae8f900d847e126d8b4e86a0ad6a46c65
jcj <jcj@163.com>
2022-09-18 17:40:38 +0800

    添加信息列表展示

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
+import axios from "axios"
 import { BrowserRouter as Router, useNavigate, Route, Navigator, useLocation, Routes, Link } from 'react-router-dom'
+import { Button, message } from 'antd'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { faMarkdown } from '@fortawesome/free-brands-svg-icons'
@@ -8,6 +10,9 @@ import useKeyPress from '../hooks/useKeyPress'
 import useContextMenu from '../hooks/useContextMenu'
 import { getParentNode } from '../utils/helper'
 import "./css/JcjIndex.scss"
+const Store = window.require('electron-store')
+const userStore = new Store({name: 'userStore'})
+var baseUrl = "https://api-vbox.jpqapro.com"
 
 
 //const navigate=useNavigate();
@@ -29,8 +34,33 @@ const Index = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
       onFileDelete(editItem.id)
     }
   }
-  const selectItem = (index)=>{
-    console.log("aaaa----",index)
+  const getData = (index) => {
+    console.log(index)
+    let url = baseUrl + "/api/merchant/message/price"
+    const user = userStore.get('user')
+    console.log(22222222222)
+    console.log(user)
+   axios({
+        url,
+        data:{},
+        method: 'POST',
+        responseType: 'stream',
+        headers: {'Session-Id': user.token}
+      }).then(response => {
+        console.log(response)
+
+        if(response.data.resultCode != "00000"){
+          message.info(response.data.returnMsg)
+        }else{
+          response.data.data.time = new Date().getTime()
+        }
+
+    }).catch(err => {
+          message.info(err.message)
+    })
+  }
+  const selectItem = (index) => {
+    console.log("aaaa----", index)
     setActive(index)
   }
   const clickedItem = useContextMenu([
@@ -86,6 +116,7 @@ const Index = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
   useEffect(() => {
     console.log("设置了active")
     console.log(active)
+    getData(active)
   }, [active])
   return (
     <div class="main1">
@@ -134,7 +165,7 @@ const Index = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
               <div class="wrap-jcj">
                 <div class="image">
                 </div>
-                <div class="text" onClick={()=>{selectItem(1)}}>价格提醒</div>
+                <div class="text" onClick={() => { selectItem(1) }}>价格提醒</div>
                 <div className={`${active}` == 1 ? 'line active' : 'line'}></div>
               </div>
             </div>
@@ -142,7 +173,7 @@ const Index = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
               <div class="wrap-jcj">
                 <div class="image">
                 </div>
-                <div class="text" onClick={()=>{selectItem(2)}} >特价商品</div>
+                <div class="text" onClick={() => { selectItem(2) }} >特价商品</div>
                 <div className={`${active}` == 2 ? 'line active' : 'line'}></div>
               </div>
             </div>
@@ -150,7 +181,7 @@ const Index = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
               <div class="wrap-jcj">
                 <div class="image">
                 </div>
-                <div class="text" onClick={()=>{selectItem(3)}} >订单通知</div>
+                <div class="text" onClick={() => { selectItem(3) }} >订单通知</div>
                 <div className={`${active}` == 3 ? 'line active' : 'line'}></div>
               </div>
             </div>
@@ -158,8 +189,40 @@ const Index = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
               <div class="wrap-jcj">
                 <div class="image">
                 </div>
-                <div class="text" onClick={()=>{selectItem(4)}} >其它</div>
+                <div class="text" onClick={() => { selectItem(4) }} >其它</div>
                 <div className={`${active}` == 4 ? 'line active' : 'line'}></div>
+              </div>
+            </div>
+          </div>
+          <div class="row-content bd-highlight">
+            <div class="wrapper">
+              <div class="item">
+                <div class="content-img">
+                  <img src="images/3.png" />
+                </div>
+                <div class="content-text">
+                  <div class="title">供应商消息</div>
+                  <div class="article">
+                    <p>供供应应商消息供应商消息供应商消息应商消息供应商消息供应商消息应商消息供应商消息供应商消息商消息供应商消息供应商消息供应商消息供应商消息供应商消息供应商消息应商消ddsdfsdsdfsff息</p>
+                  </div>
+                </div>
+                <div class="content-bigimg">
+                  <img src="images/2.png" />
+                </div>
+              </div>
+            </div>
+            <div class="wrapper">
+              <div class="item">
+                <div class="content-img">
+                  <img src="images/3.png" />
+                </div>
+                <div class="content-text">
+                  <div class="title">供应商消息</div>
+                  <div class="article">供供应商消息供应商消息供应商消息供应商消息供应商消息供应商消息供应商消息应商消息</div>
+                </div>
+                <div class="content-bigimg">
+                  <img src="images/2.png" />
+                </div>
               </div>
             </div>
           </div>
